824d60f1925f01b0efeda18292d10c28811f27b9
Ole André Vadla Ravnås <oleavr@frida.re>
2026-05-24 23:07:04 +0200

    Add convenience default export

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,10 @@
+import { DEFAULT_ISA_MASK, Scanner } from "./scanner/index.js";
+import { resolveTarget } from "./symbols.js";
+import { Target } from "./target.js";
+
 export { DEFAULT_ISA_MASK, Hit, Scanner } from "./scanner/index.js";
 export { ProcessMemory, Region, RegionFilter } from "./process/types.js";
 export { resolveTarget } from "./symbols.js";
 export { FindOpts, ScanHit, Target, TargetSpec } from "./target.js";
+
+export default { DEFAULT_ISA_MASK, Scanner, Target, resolveTarget };
